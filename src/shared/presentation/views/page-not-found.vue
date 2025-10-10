ca4bdbdc5d0fc38483dd8aab0fd9365f10d8d18c
DiegoLlamozas <diegollamozas911@gmail.com>
2025-10-10 02:22:27 -0500

    feat(views): create shared application views

---
+++ src/shared/presentation/views/page-not-found.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="not-found">
+    <h1>{{ $t('notFound.title') }}</h1>
+    <p>{{ $t('notFound.description') }}</p>
+    <pv-button
+        :label="$t('notFound.goHome')"
+        @click="$router.push('/')" />
+  </div>
+</template>
+
+<script setup>
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
+</script>
+
+<style scoped>
+.not-found {
+  text-align: center;
+  padding: 2rem;
+}
+</style>
