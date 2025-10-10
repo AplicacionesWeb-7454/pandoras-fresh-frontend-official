cb5c5ddd072da92e923f56f5143d0be360bfe57f
AlexBoo578 <alexyoshirby@hotmail.com>
2025-10-10 01:29:38 -0500

    feat(main): integrate Vue Router and Pinia for state management

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import router from './router'
+import { createPinia } from 'pinia'
 
-import pinia from "./pinia.js";
+const app = createApp(App)
+const pinia = createPinia()
 
-
-createApp(App)
-    .use(pinia)
-    .mount('#app')
+app.use(router)
+app.use(pinia)
+app.mount('#app')
