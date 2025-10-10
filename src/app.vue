9c2b2c21b18b88ab69b694a03eefc1733f872893
AlexBoo578 <alexyoshirby@hotmail.com>
2025-10-10 01:27:40 -0500

    feat(app): rename App.vue to app.vue and restructure template

---
+++ src/app.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+#app {
+  font-family: Arial, sans-serif;
+  width: 100%;
+  min-height: 100vh;
+}
+
+html, body {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
