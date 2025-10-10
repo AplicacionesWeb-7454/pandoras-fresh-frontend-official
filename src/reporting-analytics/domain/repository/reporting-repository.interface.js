5efcc2ff7ddf4b46aa3c561b99bfb9e730298082
AlexBoo578 <alexyoshirby@hotmail.com>
2025-10-10 01:30:22 -0500

    feat(reporting-repository): add ReportingRepositoryInterface for report and dashboard management

---
+++ src/reporting-analytics/domain/repository/reporting-repository.interface.js
@@ -0,0 +1,29 @@
+export class ReportingRepositoryInterface {
+    async saveReport(report) {
+        throw new Error('Method not implemented');
+    }
+
+    async findReportById(id) {
+        throw new Error('Method not implemented');
+    }
+
+    async findAllReports() {
+        throw new Error('Method not implemented');
+    }
+
+    async deleteReport(id) {
+        throw new Error('Method not implemented');
+    }
+
+    async saveDashboard(dashboard) {
+        throw new Error('Method not implemented');
+    }
+
+    async findDashboardById(id) {
+        throw new Error('Method not implemented');
+    }
+
+    async getDashboardData() {
+        throw new Error('Method not implemented');
+    }
+}
